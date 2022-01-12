77e53f4009e89e417ca20a8cb3f5900262b698fd
evan812 <61596243+evan812@users.noreply.github.com>
2022-01-12 17:18:55 -0600

    Clases and Objects

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Course {
+    private String subject;
+    private Teacher teacher;
+    private ArrayList<Student> students;
+}
--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,18 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+//        Student data = new Student();
+
+//        data.setName("Evan");
+//        data.setStudentId(01);
+//        data.setNumberOfCredits(1);
+//        data.setGpa(4.0);
+
+        Student name = new Student("Evan",01,1,4.0 );
+
+        System.out.println("Name: " + name.getName());
+        System.out.println("Student ID: " + name.getStudentId());
+        System.out.println("Number of credits: " + name.getNumberOfCredits());
+        System.out.println("GPA: " + name.getGpa());
     }
 }
