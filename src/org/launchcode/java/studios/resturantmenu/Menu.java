e953bd52ea7173c14eeb07d74e8e8c78856365c5
evan812 <61596243+evan812@users.noreply.github.com>
2022-01-24 18:40:04 -0600

    Exercise unit testing

--- src/org/launchcode/java/studios/resturantmenu/Menu.java
+++ src/org/launchcode/java/studios/resturantmenu/Menu.java
@@ -20,14 +20,19 @@ public class Menu {
         this.menuItems.add(item);
     }
 
+    public void removeItem(MenuItem item){
+        this.lastUpdate = new Date();
+        this.menuItems.remove(item);
+    }
+
     public MenuItem getNewest(){
         return menuItems.get(menuItems.size()-1);
     }
 
 
-//    MenuItem item = new MenuItem(17.50, "Hearty Steak Soup", "Main course");
-
-
-
+    MenuItem soup = new MenuItem(17.50, "Hearty Steak Soup", "Main course");
+    MenuItem wings = new MenuItem(10.98, "Spicy Wings ", "Appetizer");
+    MenuItem cake = new MenuItem(8.50, "Lava Cake", "Dessert");
+    MenuItem salad = new MenuItem(6.45, "Side Salad", "Appetizer");
 
 }
---
+++ src/org/launchcode/java/studios/resturantmenu/Restaurant.java
@@ -0,0 +1,7 @@
+package org.launchcode.java.studios.resturantmenu;
+
+public class Restaurant {
+    public static void main(String[] args) {
+
+    }
+}
