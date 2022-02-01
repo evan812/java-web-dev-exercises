9530fa9005a6364b4e48f34620a97a7eafaac99b
evan812 <61596243+evan812@users.noreply.github.com>
2022-01-31 18:11:31 -0600

    Ice Crean Ex

---
+++ src/org/launchcode/java/studios/quizzes/MultipleChoice.java
@@ -0,0 +1,4 @@
+package org.launchcode.java.studios.quizzes;
+
+//public class MultipleChoice extends Question{
+//}
---
+++ src/org/launchcode/java/studios/quizzes/Question.java
@@ -0,0 +1,14 @@
+package org.launchcode.java.studios.quizzes;
+
+public class Question {
+
+    private String question;
+    private double answer;
+
+    public Question(String question, double answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+
+}
